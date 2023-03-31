5402d6fa0907260a40cf1aa23dbd5fc19068a7fe
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-03-31 10:27:59 +0300

    lab 3 40 000 000

--- Lab3/lab3.sql
+++ Lab3/lab3.sql
@@ -11,12 +11,12 @@ FROM cities
 WHERE population > 100000
 ORDER BY name;
 
-SELECT name, population, CONCAT((population/4000000*100),'%') AS expanded_popul
+SELECT name, population, CONCAT((population/40000000*100),'%') AS expanded_popul
 FROM cities
 WHERE population > 100000
 ORDER BY population DESC;
 
-SELECT name, CONCAT((population/4000000*100),'%') AS expanded_popul
+SELECT name, CONCAT((population/40000000*100),'%') AS expanded_popul
 FROM cities
-WHERE (population/4000000*100) >= 0.1
-ORDER BY (population/4000000*100) DESC;
+WHERE (population/40000000*100) >= 0.1
+ORDER BY (population/40000000*100) DESC;
