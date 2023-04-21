4a2da6d753bdcec6df1fff7a69a932c1e7bb0f80
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-04-21 15:06:40 +0300

    lab 6 Metro

---
+++ lab6/m_branch.sql
@@ -0,0 +1,11 @@
+CREATE TABLE m_branch (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+  color VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL
+);
+
+INSERT INTO m_branch (id, name, color) VALUES
+(3, 'Олексіївська', 'зелена'),
+(2, 'Салтівська', 'блакитна'),
+(1, 'Холодногірсько-Заводська', 'червона');
+
