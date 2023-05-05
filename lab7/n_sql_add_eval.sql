607830e6f828f242f17dc3ecbc985b9682cbbfcd
MaksimSaprykin <Maksym.Saprykin@infiz.khpi.edu.ua>
2023-05-05 13:19:02 +0300

    dell sql add eval.qsl

--- lab7/n_sql_add_eval.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO n_news_evaluation (evaluation, date_evaluation, ip_address, news_id)
-VALUES (4, NOW(), '000.000.000.000', 1)
-WHERE NOT EXISTS (
-  SELECT id FROM n_news_evaluation 
-  WHERE ip_address = '000.000.000.000' AND news_id = 1
-);
-
-
